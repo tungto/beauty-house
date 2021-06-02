e03a18a6ea76d971b356505d5ce6bda05d7b1bf2
Tung To <sifebuh.totung@gmail.com>
2021-06-02 14:50:44 +0700

    style empty cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { CartContainer, CartTotal } from '../components';
 import { useCartContext } from '../context/cart_context';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 const Cart = () => {
   const { cart } = useCartContext();
   if (cart.length < 1) {
     return (
-      <div className='container'>
-        <h1>Your cart is empty</h1>
-        <Link className='btn' to='/api/products'>
-          fill it
-        </Link>
-      </div>
+      <Wrapper>
+        <div className='empty-cart section'>
+          <h1>Your cart is empty</h1>
+          <Link className='btn btn-fill' to='/api/products'>
+            fill it
+          </Link>
+        </div>
+      </Wrapper>
     );
   }
   return (
@@ -21,5 +24,16 @@ const Cart = () => {
     </div>
   );
 };
+
+const Wrapper = styled.div`
+  text-align: center;
+  min-height: calc(80vh + 3rem);
+  .btn-fill {
+    width: 10rem;
+    padding: 1rem;
+    background: lightblue;
+    margin: 5rem;
+  }
+`;
 
 export default Cart;
